0b8434cafa96e49547de85b2c04496320e37a07d
cjq002 <692771080@qq.com>
2015-02-11 17:14:48 +0800

    Initial Commit

---
+++ UIActionSheet动作表/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIActionSheet动作表
+//
+//  Created by 陈家庆 on 15-2-11.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
